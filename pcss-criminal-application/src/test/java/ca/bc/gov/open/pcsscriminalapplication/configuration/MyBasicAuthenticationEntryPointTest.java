5b2418fd7585e73a1a52e5026ec243bbf2e68ab1
Brendan Beach <55457785+BrendanBeachBC@users.noreply.github.com>
2021-10-21 16:38:31 -0700

    Added unit tests for configuration package

---
+++ pcss-criminal-application/src/test/java/ca/bc/gov/open/pcsscriminalapplication/configuration/MyBasicAuthenticationEntryPointTest.java
@@ -0,0 +1,54 @@
+package ca.bc.gov.open.pcsscriminalapplication.configuration;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.*;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.test.util.ReflectionTestUtils;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@DisplayName("Auth Entry Point Test Suite")
+public class MyBasicAuthenticationEntryPointTest {
+
+    @Mock
+    HttpServletRequest mockRequest;
+
+    @Mock
+    HttpServletResponse mockResponse;
+
+    @Mock
+    AuthenticationException authExMock;
+
+    @Mock
+    PrintWriter mockWriter;
+
+    @Mock
+    ObjectMapper mockObjectMapper;
+
+    private MyBasicAuthenticationEntryPoint sut;
+
+    @BeforeAll
+    public void beforeAll() {
+        MockitoAnnotations.openMocks(this);
+
+        sut = new MyBasicAuthenticationEntryPoint();
+    }
+
+    @Test
+    @DisplayName("Test commence method")
+    public void testCommence() throws IOException {
+        Mockito.when(mockRequest.getRequestURI()).thenReturn("request URI");
+        Mockito.when(mockResponse.getWriter()).thenReturn(mockWriter);
+        ReflectionTestUtils.setField(sut, "objectMapper", mockObjectMapper);
+
+        Assertions.assertDoesNotThrow(() -> sut.commence(mockRequest, mockResponse, authExMock));
+    }
+}
