a9ff98063046d4fa36c83961df20cf632031fcbf
TayGov <taylor.clausen@gov.bc.ca>
2021-11-16 08:53:00 -0800

    Moar testing

---
+++ pcss-criminal-application/src/test/java/service/appearancevalidatorimpl/ValidateSetAppearanceCriminalTest.java
@@ -0,0 +1,9 @@
+package service.appearancevalidatorimpl;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.TestInstance;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@DisplayName("ValidateSetAppearanceCriminal Test")
+public class ValidateSetAppearanceCriminalTest {
+}
